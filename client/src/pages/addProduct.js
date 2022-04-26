a8e4fd703bcbf56f3de3d95153c36dcf5849493f
Jacky <ljjjun5845@gmail.com>
2022-04-26 15:55:23 +0800

    update add product UI'

--- client/src/pages/addProduct.js
+++ client/src/pages/addProduct.js
@@ -1,37 +1,21 @@
 import React from "react";
+import ComponentDropzone from "../components/dropzone";
 
 class AddProduct extends React.Component {
-	state = { name: "", price: "", image: { file: "", blob: "", base64: "" }, stackId: null };
+	state = { name: "", price: "", imageFile: "", stackId: null };
 
-	selectedImage = (e) => {
-		const file = e.target.files[0];
-		const blob = URL.createObjectURL(file);
-
-		let reader = new FileReader()
-		reader.readAsDataURL(file)
-		reader.onload = () => {
-			this.setState({
-				image: {
-					file,
-					blob,
-					base64: reader.result
-				}
-			})
-		};
-		reader.onerror = function (error) {
-			console.log('Error: ', error);
-		}
+	selectedImage = (imageFiles) => {
+		this.setState({ imageFile: imageFiles[0] })
 	}
 
 	addItem = () => {
 		const { drizzle, account } = this.props;
-		const { name, price, image } = this.state;
-		const { file } = image;
+		const { name, price, imageFile } = this.state;
 		const contract = drizzle.contracts.P2P;
 		const self = this;
 
 		const formData = new FormData()
-		formData.append('image', file)
+		formData.append('image', imageFile)
 		fetch('http://127.0.0.1:3001/upload/image', {
 			method: 'POST',
 			body: formData
@@ -73,39 +57,42 @@ class AddProduct extends React.Component {
 	};
 
 	render() {
-		const { image } = this.state;
-		const { blob } = image;
 		return (
 			<>
-				<div className="row mt-5 justify-content-around">
-					<div className="col-auto">
-						<label className="form-label">Name</label>
+				<div className="row">
+					<div className="col-6">
+						<p>I’m proud of you. Way to go. You might be interested in NFTs because it gives you a way to sell work that there otherwise might not be much of a market for. If you come up with a really cool digital sticker idea.</p>
+						<p>Also, NFTs have a feature that you can enable that will pay you every time the NFT is sold, making sure that if your work gets super popular and balloons in value, you’ll see some of that benefit.</p>
 					</div>
-					<div className="col-auto">
-						<input className="form-control" type="text" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
-					</div>
-					<div className="col-auto">
-						<label className="form-label">Amount</label>
-					</div>
-					<div className="col-auto">
-						<div className="input-group">
-							<input className="form-control" type="number" value={this.state.price} onChange={(e) => this.setState({ price: e.target.value })} />
-							<span className="input-group-text">ETH</span>
+					<div className="col-6">
+						<div className="mb-3 row">
+							<ComponentDropzone
+								accept="image/*"
+								title="Drag 'n' drop image here, or click to select image"
+								onDrop={this.selectedImage}
+							/>
 						</div>
-					</div>
-					<div className="col-auto">
-						<button onClick={this.addItem} className="btn btn-primary">Add</button>
-					</div>
-				</div>
-				<div className="row mt-3 justify-content-between">
-					<div className="col-4">
-						<input className="form-control" type="file" accept="image/*" onChange={this.selectedImage} />
-					</div>
-					<div className="col-3">
-						{blob && <img src={blob || ""} className="w-100" />}
-					</div>
-					<div className="col-12">
-						<p>{this.getTxStatus()}</p>
+						<div className="mb-3 row">
+							<label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
+							<div className="col-sm-10">
+								<input type="text" className="form-control" id="inputName" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
+							</div>
+						</div>
+						<div className="mb-3 row">
+							<label htmlFor="inputAmount" className="col-sm-2 col-form-label">Amount</label>
+							<div className="col-sm-10">
+								<div className="input-group">
+									<input className="form-control" type="number" id="inputAmount" value={this.state.price} onChange={(e) => this.setState({ price: e.target.value })} />
+									<span className="input-group-text">ETH</span>
+								</div>
+							</div>
+						</div>
+						<div className="d-flex float-right">
+							<button onClick={this.addItem} className="btn btn-primary">Add</button>
+						</div>
+						<div>
+							<p>{this.getTxStatus()}</p>
+						</div>
 					</div>
 				</div>
 			</>
